ed8952b19785d036e417be56ab723c7dcb4de0ca
maciekkowalczyk <maciek.kowalczyk@yandex.com>
2016-05-14 21:37:38 +0000

    add conditional statements to idnex action and view file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,11 @@
 class ItemsController < ApplicationController
     
     before_action :find_item, only: [:show, :edit, :update, :destroy]
-    
+    # before_action
     def index
-        @items = Item.all.order("created_at DESC")
+        if user_signed_in?
+            @items = Item.where(user_id: current_user.id).order("created_at DESC")
+        end 
     end
     
     def show
